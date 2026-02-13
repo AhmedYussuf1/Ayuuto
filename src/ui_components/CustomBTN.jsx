8cb4af85bdd708d038cfb63f01e94d40a535b77e
AhmedYu <abyan-adan@hotmail.com>
2026-02-12 20:13:34 -0600

    modified nothing

--- src/ui_components/CustomBTN.jsx
+++
@@ -1,9 +0,0 @@
- 
- function CustomBTN({value, onClick, customStyle}) {
-  return(
-    <button className={customStyle} onClick={onClick}>
-      {value}
-    </button>
-   );
- }
-  export default CustomBTN;
