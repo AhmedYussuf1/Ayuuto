caf9a366172e7c5de1d0881267bf8a7c6762eaca
AhmedYu <abyan-adan@hotmail.com>
2026-02-28 21:55:40 -0600

    moved all the front-end code into front-end folder, so that we can separate it from the back-end cod

--- src/pages/LogoIn.jsx
+++
@@ -1,6 +0,0 @@
-export default function LogoIn(){
-    return(<div className=" logo  container grid bg-alert">
-        Login Page 
-
-    </div>)
-}
--- src/ui_components/CustomInPut.jsx
+++
@@ -1,7 +0,0 @@
-let CustomInPut = ({typeOfInput , styleOfInput,  placeHolder})=>{
-    return(
-       <input type={typeOfInput} className={styleOfInput} placeholder={placeHolder}/>
-    );
-
-}
- export default CustomInPut; 
