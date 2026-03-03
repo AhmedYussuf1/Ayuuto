8d1c256ea26362dd3434ff7afbb1979723dcc281
AhmedYu <abyan-adan@hotmail.com>
2026-03-03 10:29:59 -0600

    working on back-end connection to PostgreSQL

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,14 +1,15 @@
 import express  from 'express';
+import admin from 'firebase-admin';
+import {Client, Clinet} from 'pg';
  const app = express();
+ const con = new Client({
+   host: "localhost", user:"pstgres", port : 5432
+ })
  app.use(express.json);
-  app.get( '/hello', function(req, res){
-    res.send("HELLO!");
-
- });
-
- app.post('/hello', function (req, res){
-req.send("HELLO FRO POST" + req.body.name) });
- app.listen(8000, function(){
-    console.log("server is listening to port 8000");
-    
- });
+ 
+ let db;
+ async funciton connectToDB(){
+   const usri = '';
+   const
+ }
+ 
