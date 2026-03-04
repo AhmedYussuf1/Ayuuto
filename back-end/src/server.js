8a800e9b3e9a32b18ffd2107eabcdf4665590985
AhmedYu <abyan-adan@hotmail.com>
2026-03-04 09:39:11 -0600

    fixed server.js. it was broken due gitHub mishub

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,4 +1,15 @@
- function test(){
-  console.log("testing");
- }
- test();
+import express from "express";
+import p from "./dbConnection.js";
+console.log("testing");
+app.get("/dbtest", async (req, res, next) => {
+  try {
+    const r = await pool.query("SELECT NOW() as now");
+    res.json(r.rows[0]);
+  } catch (e) {
+    next(e);
+  }
+});
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: err.message });
+});
