232b9673276aa5eb14a9fcedaedf8cfc044815dc
AhmedYu <abyan-adan@hotmail.com>
2026-03-05 16:50:55 -0600

    added firebase credentials to the server.js

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,17 @@
   import express from "express";
  import p from "./dbConnection.js";
+ import admin from "firebase-admin";
+ import fs from 'fs'
+ const credential = JSON.parse(fs.readFileSync("../firebase_cr.json"));
+
  //  membership
    const app = express();
 app.use(express.json());
+app.use(cors({ origin: ["http://localhost:3000", "http://localhost:5173"] }));
 
+admin.initializeApp({
+  credential: admin.credential.cert(credential);
+});
 app.get("/", (req, res) => res.send("API running"));
 
 app.get("/member", async (req, res) => {
